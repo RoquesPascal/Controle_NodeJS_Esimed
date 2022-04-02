0ea4423ba7f38d1809f8af278727091c3f7650fc
pasca <pascal>
2022-04-02 18:15:52 +0200

    Lot 1 - Personnes rencontrées et rencontres : - Ajout de la modification d'une rencontre

--- src/controllers/rencontre.routes.js
+++ src/controllers/rencontre.routes.js
@@ -38,13 +38,13 @@ router.post('/',
         let dateDeNaissance = new Date(req.body.dateNaissanceAnnee, req.body.dateNaissanceMois - 1, req.body.dateNaissanceJour);
         let dateDeLaRencontre = new Date(req.body.dateRencontreAnnee, req.body.dateRencontreMois - 1, req.body.dateRencontreJour);
         const rencontre = await Table_Rencontres.create({id            : uuid.v4(),
-                                                                 nom           : req.body.nom,
-                                                                 prenom        : req.body.prenom,
-                                                                 sexe          : req.body.sexe,
-                                                                 dateNaissance : dateDeNaissance,
-                                                                 dateRencontre : dateDeLaRencontre,
-                                                                 note          : req.body.note,
-                                                                 commentaire   : req.body.commentaire});
+                                                               nom           : req.body.nom,
+                                                               prenom        : req.body.prenom,
+                                                               sexe          : req.body.sexe,
+                                                               dateNaissance : dateDeNaissance,
+                                                               dateRencontre : dateDeLaRencontre,
+                                                               note          : req.body.note,
+                                                               commentaire   : req.body.commentaire});
 
         const tokenDecode = jwtDecode(req.headers.authorization);
         await Table_SessionsRencontres.create({id            : uuid.v4(),
@@ -54,17 +54,50 @@ router.post('/',
     }
     catch(e)
     {
-        res.status(409).send("Erreur lors de l'ajout de la rencontre");
+        res.status(400).send("Erreur lors de l'ajout de la rencontre");
     }
 });
 
-router.put('/', async (req, res) => {
-    await Table_Utilisateurs.update({ lastName: "Doe" }, {
-        where: {
-            firstName: "Jean"
-        }
-    });
-    res.status(204).end();
+router.put('/',
+           body('id').isString().notEmpty(),
+           body('nom').isString().notEmpty(),
+           body('prenom').isString().notEmpty(),
+           body('sexe').isInt().notEmpty(),
+           body('dateRencontreJour').isInt().notEmpty(),
+           body('dateRencontreMois').isInt().notEmpty(),
+           body('dateRencontreAnnee').isInt().notEmpty(),
+           body('note').isInt(),
+           async (req, res) =>
+{
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+        return res.status(400).json({ errors: errors.array() });
+
+    try
+    {
+        let dateDeNaissance = new Date(req.body.dateNaissanceAnnee, req.body.dateNaissanceMois - 1, req.body.dateNaissanceJour);
+        let dateDeLaRencontre = new Date(req.body.dateRencontreAnnee, req.body.dateRencontreMois - 1, req.body.dateRencontreJour);
+        await Table_Rencontres.update(
+            {
+                nom           : req.body.nom,
+                prenom        : req.body.prenom,
+                sexe          : req.body.sexe,
+                dateNaissance : dateDeNaissance,
+                dateRencontre : dateDeLaRencontre,
+                note          : req.body.note,
+                commentaire   : req.body.commentaire
+            }, {
+            where :
+            {
+                id : req.body.id
+            }
+        });
+        res.status(204).send("Rencontre modifiée !");
+    }
+    catch(e)
+    {
+        res.status(400).send("Erreur lors de la modification de la rencontre");
+    }
 });
 
 router.delete('/', async (req, res) => {
