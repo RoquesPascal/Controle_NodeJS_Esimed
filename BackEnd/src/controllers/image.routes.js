4f573852c36a209ad92d9b84649c6110bc3df5b4
pasca <pascal>
2022-06-03 09:02:18 +0200

    Lot 3 - Image des personnes à rencontrer :
    Modif des routes pour mettre une sécurité

--- BackEnd/src/controllers/image.routes.js
+++ BackEnd/src/controllers/image.routes.js
@@ -5,8 +5,30 @@ const multer       = require('multer');
 const uuid         = require("uuid");
 const upload       = multer({ dest: 'uploads/' });
 const Table_Images = require("../models/image.model");
+const Table_PersonnesARencontrer = require("../models/personnes-a-rencontrer.model");
+const jwtDecode = require("jwt-decode");
+const Table_RelationCreationUtilisateurPersonnesARencontrer = require("../models/relation-creation-utilisateur-personne-a-rencontrer.model");
 
 
+
+router.get('/:id', async (req, res) =>
+{
+    try
+    {
+        const image = await Table_Images.findOne({
+            where :
+                {
+                    id : req.params.id
+                }
+        });
+
+        res.download(`${image.chemin + image.nomUnique}`, image.nomOriginal);
+    }
+    catch(e)
+    {
+        res.status(500).send(`erreur de la récupération de l'image`);
+    }
+})
 
 router.post('/personne-rencontree/:idPersonneRencontree',
             upload.single('file'),
@@ -14,6 +36,27 @@ router.post('/personne-rencontree/:idPersonneRencontree',
 {
     try
     {
+        const tokenDecode = jwtDecode(req.headers.authorization);
+        const relationUtilisateurPersonnes = await Table_RelationCreationUtilisateurPersonnesARencontrer.findOne({
+            where :
+            {
+                idUtilisateur        : tokenDecode.id,
+                idPersonneRencontree : req.params.id
+            }
+        });
+
+        const personne = await Table_PersonnesARencontrer.findOne({
+            where :
+            {
+                id : relationUtilisateurPersonnes.idPersonneRencontree
+            }
+        });
+
+        if((personne == null) || (personne.id == null))
+            res.status(403).send(`Vous n'avez pas le droit de créer une image pour cette ressource`);
+
+
+
         const image = req.file;
 
         if(!image)
@@ -31,27 +74,8 @@ router.post('/personne-rencontree/:idPersonneRencontree',
     }
     catch(e)
     {
-        res.status(500).send(`erreur de la creation de l'image`);
+        res.status(500).send(`erreur de la création de l'image`);
     }
 });
-
-router.get('/:id', async (req, res) =>
-{
-    try
-    {
-        const image = await Table_Images.findOne({
-            where :
-            {
-                id : req.params.id
-            }
-        });
-
-        res.download(`${image.chemin + image.nomUnique}`, image.nomOriginal);
-    }
-    catch(e)
-    {
-        res.status(500).send(`erreur de la recuperation de l'image`);
-    }
-})
 
 exports.initializeRoutes = () => router;
